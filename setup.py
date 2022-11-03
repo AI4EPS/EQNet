a7ef60f0d4fcb03a67399e31251429d6b437d52b
Weiqiang <zhuwq0@gmail.com>
2022-11-03 13:30:22 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="EQNet",
-    version="0.1.0",
+    version="0.1.1",
     long_description="EQNet: Neural Network Models for Earthquakes",
     long_description_content_type="text/markdown",
     packages=["eqnet"],
