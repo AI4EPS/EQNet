7e539df53c60d449797f13f7ad5012eb9a1067fc
Weiqiang <zhuwq0@gmail.com>
2022-11-03 13:22:58 -0700

    add documentation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="EQNet",
+    version="0.1.0",
+    long_description="EQNet: Neural Network Models for Earthquakes",
+    long_description_content_type="text/markdown",
+    packages=["eqnet"],
+    install_requires=["torch", "torchvision", "h5py", "matplotlib", "pandas"],
+)
