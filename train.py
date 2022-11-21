111ebcae7522f292e40c13e7c4fe78b0e1e91303
Weiqiang <zhuwq0@gmail.com>
2022-11-20 17:06:47 -0800

    update format

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 import datetime
 import logging
 import os
-import time
 import random
+import time
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -21,14 +21,12 @@ from eqnet.data import (
     SeismicNetworkIterableDataset,
     SeismicTraceIterableDataset,
 )
-from eqnet.models import log_transform, normalize_local
+from eqnet.models import normalize_local
 
 torch.manual_seed(0)
 random.seed(0)
 np.random.seed(0)
-
 matplotlib.use("agg")
-
 logger = logging.getLogger("EQNet")
 
 
@@ -46,7 +44,9 @@ def evaluate(model, data_loader, device, num_classes=3, args=None):
                 phase = F.softmax(out["phase"], dim=1).cpu()
                 event = torch.sigmoid(out["event"]).cpu()
                 print("Plotting...")
-                eqnet.utils.visualize_phasenet_train(meta, phase, event, epoch=i, figure_dir=args.output_dir)
+                eqnet.utils.visualize_phasenet_train(
+                    meta, phase, event, epoch=i, figure_dir=args.output_dir
+                )
                 del phase, event
 
             if args.model == "deepdenoiser":
@@ -58,13 +58,17 @@ def evaluate(model, data_loader, device, num_classes=3, args=None):
                 preds = model(meta)
                 preds = F.softmax(preds, dim=1).cpu()
                 print("Plotting...")
-                eqnet.utils.visualize_das_train(meta, preds, epoch=i, figure_dir=args.output_dir)
+                eqnet.utils.visualize_das_train(
+                    meta, preds, epoch=i, figure_dir=args.output_dir
+                )
                 del preds
 
             elif args.model == "autoencoder":
                 preds = model(meta).cpu()
                 print("Plotting...")
-                eqnet.utils.visualize_autoencoder_das_train(meta, preds, epoch=i, figure_dir=args.output_dir)
+                eqnet.utils.visualize_autoencoder_das_train(
+                    meta, preds, epoch=i, figure_dir=args.output_dir
+                )
                 del preds
 
             elif args.model == "eqnet":
@@ -72,7 +76,9 @@ def evaluate(model, data_loader, device, num_classes=3, args=None):
                 phase = F.softmax(out["phase"], dim=1).cpu()
                 event = torch.sigmoid(out["event"]).cpu()
                 print("Plotting...")
-                eqnet.utils.visualize_eqnet_train(meta, phase, event, epoch=i, figure_dir=args.output_dir)
+                eqnet.utils.visualize_eqnet_train(
+                    meta, phase, event, epoch=i, figure_dir=args.output_dir
+                )
                 del phase, event
 
             i += 1
@@ -85,7 +91,16 @@ def evaluate(model, data_loader, device, num_classes=3, args=None):
 
 
 def train_one_epoch(
-    model, optimizer, data_loader, lr_scheduler, device, epoch, iters_per_epoch, print_freq, scaler=None, args=None
+    model,
+    optimizer,
+    data_loader,
+    lr_scheduler,
+    device,
+    epoch,
+    iters_per_epoch,
+    print_freq,
+    scaler=None,
+    args=None,
 ):
     model.train()
     metric_logger = utils.MetricLogger(delimiter="  ")
@@ -128,7 +143,9 @@ def train_one_epoch(
             # phase, event = None, None
             polarity = torch.sigmoid(out["polarity"]).cpu()
             print("Plotting...")
-            eqnet.utils.visualize_phasenet_train(meta, phase, event, polarity, epoch=epoch, figure_dir=args.output_dir)
+            eqnet.utils.visualize_phasenet_train(
+                meta, phase, event, polarity, epoch=epoch, figure_dir=args.output_dir
+            )
             del phase, event
 
         if args.model == "deepdenoiser":
@@ -138,13 +155,17 @@ def train_one_epoch(
             preds = model(meta)
             preds = F.softmax(preds, dim=1).cpu()
             print("Plotting...")
-            eqnet.utils.visualize_das_train(meta, preds, epoch=epoch, figure_dir=args.output_dir)
+            eqnet.utils.visualize_das_train(
+                meta, preds, epoch=epoch, figure_dir=args.output_dir
+            )
             del preds
 
         elif args.model == "autoencoder":
             preds = model(meta).cpu()
             print("Plotting...")
-            eqnet.utils.visualize_autoencoder_das_train(meta, preds, epoch=epoch, figure_dir=args.output_dir)
+            eqnet.utils.visualize_autoencoder_das_train(
+                meta, preds, epoch=epoch, figure_dir=args.output_dir
+            )
             del preds
 
         elif args.model == "eqnet":
@@ -152,13 +173,13 @@ def train_one_epoch(
             phase = F.softmax(out["phase"], dim=1).cpu()
             event = torch.sigmoid(out["event"]).cpu()
             print("Plotting...")
-            eqnet.utils.visualize_eqnet_train(meta, phase, event, epoch=epoch, figure_dir=args.output_dir)
+            eqnet.utils.visualize_eqnet_train(
+                meta, phase, event, epoch=epoch, figure_dir=args.output_dir
+            )
             del phase, event
 
 
 def main(args):
-
-    num_classes = 3
 
     if args.output_dir:
         utils.mkdir(args.output_dir)
@@ -199,7 +220,9 @@ def main(args):
         train_sampler = None
         dataset_test = DASIterableDataset(
             # data_path="/net/kuafu/mnt/tank/data/EventData/Mammoth_south/data/",
-            label_path=["/net/kuafu/mnt/tank/data/EventData/Mammoth_south/picks_phasenet_filtered/"],
+            label_path=[
+                "/net/kuafu/mnt/tank/data/EventData/Mammoth_south/picks_phasenet_filtered/"
+            ],
             format="h5",
             training=True,
             stack_event=False,
@@ -265,26 +288,16 @@ def main(args):
         )  # , find_unused_parameters=True)
         model_without_ddp = model.module
 
-    # params_to_optimize = [
-    #     {"params": [p for p in model_without_ddp.backbone.parameters() if p.requires_grad], "lr": args.lr},
-    #     {"params": [p for p in model_without_ddp.classifier.parameters() if p.requires_grad]},
-    # ]
-    # if args.aux_loss:
-    #     params = [p for p in model_without_ddp.aux_classifier.parameters() if p.requires_grad]
-    #     params_to_optimize.append({"params": params, "lr": args.lr * 10})
-
     params_to_optimize = [p for p in model_without_ddp.parameters() if p.requires_grad]
     # optimizer = torch.optim.SGD(params_to_optimize, lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
-    optimizer = torch.optim.AdamW(params_to_optimize, lr=args.lr, weight_decay=args.weight_decay)
+    optimizer = torch.optim.AdamW(
+        params_to_optimize, lr=args.lr, weight_decay=args.weight_decay
+    )
 
     scaler = torch.cuda.amp.GradScaler() if args.amp else None
 
-    iters_per_epoch = len(data_loader)//100 * 100
+    iters_per_epoch = len(data_loader) // 100 * 100
     iters_per_epoch = 100
-    # iters_per_epoch = 1000
-    # main_lr_scheduler = torch.optim.lr_scheduler.LambdaLR(
-    #     optimizer, lambda x: (1 - x / (iters_per_epoch * (args.epochs - args.lr_warmup_epochs))) ** 0.9
-    # )
     main_lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
         optimizer, T_max=iters_per_epoch * (args.epochs - args.lr_warmup_epochs)
     )
@@ -305,14 +318,18 @@ def main(args):
                 f"Invalid warmup lr method '{args.lr_warmup_method}'. Only linear and constant are supported."
             )
         lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
-            optimizer, schedulers=[warmup_lr_scheduler, main_lr_scheduler], milestones=[warmup_iters]
+            optimizer,
+            schedulers=[warmup_lr_scheduler, main_lr_scheduler],
+            milestones=[warmup_iters],
         )
     else:
         lr_scheduler = main_lr_scheduler
 
     if args.resume:
         checkpoint = torch.load(args.resume, map_location="cpu")
-        model_without_ddp.load_state_dict(checkpoint["model"], strict=not args.test_only)
+        model_without_ddp.load_state_dict(
+            checkpoint["model"], strict=not args.test_only
+        )
         if not args.test_only:
             optimizer.load_state_dict(checkpoint["optimizer"])
             lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
@@ -331,7 +348,16 @@ def main(args):
         if args.distributed and (train_sampler is not None):
             train_sampler.set_epoch(epoch)
         train_one_epoch(
-            model, optimizer, data_loader, lr_scheduler, device, epoch, iters_per_epoch, args.print_freq, scaler, args
+            model,
+            optimizer,
+            data_loader,
+            lr_scheduler,
+            device,
+            epoch,
+            iters_per_epoch,
+            args.print_freq,
+            scaler,
+            args,
         )
         # confmat = evaluate(model, data_loader_test, device=device, args=args)
         checkpoint = {
@@ -343,8 +369,12 @@ def main(args):
         }
         if args.amp:
             checkpoint["scaler"] = scaler.state_dict()
-        utils.save_on_master(checkpoint, os.path.join(args.output_dir, f"model_{epoch}.pth"))
-        utils.save_on_master(checkpoint, os.path.join(args.output_dir, "checkpoint.pth"))
+        utils.save_on_master(
+            checkpoint, os.path.join(args.output_dir, f"model_{epoch}.pth")
+        )
+        utils.save_on_master(
+            checkpoint, os.path.join(args.output_dir, "checkpoint.pth")
+        )
 
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
@@ -354,24 +384,50 @@ def main(args):
 def get_args_parser(add_help=True):
     import argparse
 
-    parser = argparse.ArgumentParser(description="PyTorch Segmentation Training", add_help=add_help)
+    parser = argparse.ArgumentParser(
+        description="PyTorch Segmentation Training", add_help=add_help
+    )
 
     parser.add_argument("--data-path", default="./data/", type=str, help="dataset path")
     parser.add_argument("--dataset", default="", type=str, help="dataset name")
     parser.add_argument("--model", default="phasenet_das", type=str, help="model name")
-    parser.add_argument("--backbone", default="resnet50", type=str, help="model backbone")
-    parser.add_argument("--aux-loss", action="store_true", help="auxiliar loss")
-    parser.add_argument("--device", default="cuda", type=str, help="device (Use cuda or cpu Default: cuda)")
     parser.add_argument(
-        "-b", "--batch-size", default=8, type=int, help="images per gpu, the total batch size is $NGPU x batch_size"
+        "--backbone", default="resnet50", type=str, help="model backbone"
     )
-    parser.add_argument("--epochs", default=100, type=int, metavar="N", help="number of total epochs to run")
+    parser.add_argument("--aux-loss", action="store_true", help="auxiliar loss")
+    parser.add_argument(
+        "--device",
+        default="cuda",
+        type=str,
+        help="device (Use cuda or cpu Default: cuda)",
+    )
+    parser.add_argument(
+        "-b",
+        "--batch-size",
+        default=8,
+        type=int,
+        help="images per gpu, the total batch size is $NGPU x batch_size",
+    )
+    parser.add_argument(
+        "--epochs",
+        default=100,
+        type=int,
+        metavar="N",
+        help="number of total epochs to run",
+    )
 
     parser.add_argument(
-        "-j", "--workers", default=4, type=int, metavar="N", help="number of data loading workers (default: 16)"
+        "-j",
+        "--workers",
+        default=4,
+        type=int,
+        metavar="N",
+        help="number of data loading workers (default: 16)",
     )
     parser.add_argument("--lr", default=0.001, type=float, help="initial learning rate")
-    parser.add_argument("--momentum", default=0.9, type=float, metavar="M", help="momentum")
+    parser.add_argument(
+        "--momentum", default=0.9, type=float, metavar="M", help="momentum"
+    )
     parser.add_argument(
         "--wd",
         "--weight-decay",
@@ -381,13 +437,29 @@ def get_args_parser(add_help=True):
         help="weight decay (default: 1e-4)",
         dest="weight_decay",
     )
-    parser.add_argument("--lr-warmup-epochs", default=1, type=int, help="the number of epochs to warmup (default: 0)")
-    parser.add_argument("--lr-warmup-method", default="linear", type=str, help="the warmup method (default: linear)")
-    parser.add_argument("--lr-warmup-decay", default=0.01, type=float, help="the decay for lr")
+    parser.add_argument(
+        "--lr-warmup-epochs",
+        default=1,
+        type=int,
+        help="the number of epochs to warmup (default: 0)",
+    )
+    parser.add_argument(
+        "--lr-warmup-method",
+        default="linear",
+        type=str,
+        help="the warmup method (default: linear)",
+    )
+    parser.add_argument(
+        "--lr-warmup-decay", default=0.01, type=float, help="the decay for lr"
+    )
     parser.add_argument("--print-freq", default=10, type=int, help="print frequency")
-    parser.add_argument("--output-dir", default="./output", type=str, help="path to save outputs")
+    parser.add_argument(
+        "--output-dir", default="./output", type=str, help="path to save outputs"
+    )
     parser.add_argument("--resume", default="", type=str, help="path of checkpoint")
-    parser.add_argument("--start-epoch", default=0, type=int, metavar="N", help="start epoch")
+    parser.add_argument(
+        "--start-epoch", default=0, type=int, metavar="N", help="start epoch"
+    )
     parser.add_argument(
         "--test-only",
         dest="test_only",
@@ -395,23 +467,47 @@ def get_args_parser(add_help=True):
         action="store_true",
     )
     parser.add_argument(
-        "--use-deterministic-algorithms", action="store_true", help="Forces the use of deterministic algorithms only."
+        "--use-deterministic-algorithms",
+        action="store_true",
+        help="Forces the use of deterministic algorithms only.",
     )
     # distributed training parameters
-    parser.add_argument("--world-size", default=1, type=int, help="number of distributed processes")
-    parser.add_argument("--dist-url", default="env://", type=str, help="url used to set up distributed training")
+    parser.add_argument(
+        "--world-size", default=1, type=int, help="number of distributed processes"
+    )
+    parser.add_argument(
+        "--dist-url",
+        default="env://",
+        type=str,
+        help="url used to set up distributed training",
+    )
 
-    parser.add_argument("--weights", default=None, type=str, help="the weights enum name to load")
-    parser.add_argument("--weights-backbone", default=None, type=str, help="the backbone weights enum name to load")
+    parser.add_argument(
+        "--weights", default=None, type=str, help="the weights enum name to load"
+    )
+    parser.add_argument(
+        "--weights-backbone",
+        default=None,
+        type=str,
+        help="the backbone weights enum name to load",
+    )
 
     # Mixed precision training parameters
-    parser.add_argument("--amp", action="store_true", help="Use torch.cuda.amp for mixed precision training")
+    parser.add_argument(
+        "--amp",
+        action="store_true",
+        help="Use torch.cuda.amp for mixed precision training",
+    )
 
     ## Data Augmentation
     parser.add_argument("--stack-noise", action="store_true", help="Stack noise")
     parser.add_argument("--stack-event", action="store_true", help="Stack event")
-    parser.add_argument("--resample-space", action="store_true", help="Resample space resolution")
-    parser.add_argument("--resample-time", action="store_true", help="Resample time  resolution")
+    parser.add_argument(
+        "--resample-space", action="store_true", help="Resample space resolution"
+    )
+    parser.add_argument(
+        "--resample-time", action="store_true", help="Resample time  resolution"
+    )
     return parser
 
 
