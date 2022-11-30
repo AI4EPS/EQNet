fix missing normalization error

eqnet/data/seismic_trace.py
@@ -21,7 +21,7 @@ from scipy import signal
 
 def normalize(data):
     """
-    data: [3, nt, nsta]
+    data: [3, nt, nsta] or [3, nt]
     """
     data = data - data.mean(axis=1, keepdims=True)
     std = data.std(axis=1, keepdims=True)
@@ -102,7 +102,7 @@ def stack_event(
 
     max_tries = 30
     # while random.random() < 0.5:
-    for i in range(5):
+    for i in range(random.randint(1, 6)):
         tries = 0
         while tries < max_tries:
 
@@ -258,7 +258,7 @@ class SeismicTraceIterableDataset(IterableDataset):
             if not os.path.exists(tmp_hdf5_keys):
                 self.data_list = [event + "/" + station for event in fp.keys() for station in list(fp[event].keys())]
                 with open(tmp_hdf5_keys, "w") as f:
-                    for x in self.trace_ids:
+                    for x in self.data_list:
                         f.write(x + "\n")
             else:
                 self.data_list = pd.read_csv(tmp_hdf5_keys, header=None, names=["trace_id"])["trace_id"].values.tolist()
@@ -362,12 +362,20 @@ class SeismicTraceIterableDataset(IterableDataset):
         if self.hdf5_fp is None:
             hdf5_fp = h5py.File(os.path.join(self.data_path, trace_id), "r")
             event_id = "data"
-            sta_id = np.random.choice(list(hdf5_fp["data"].keys()))
-            trace_id = event_id + "/" + sta_id
+            sta_ids = list(hdf5_fp["data"].keys())
+            np.random.shuffle(sta_ids)
+            for sta_id in sta_ids:
+                trace_id = event_id + "/" + sta_id
+                waveform = hdf5_fp[trace_id][:, :].T  # [3, Nt]
+                tmp_max = np.max(np.abs(waveform), axis=1)
+                if np.all(tmp_max > 0): ## three component data
+                    break
         else:
+            hdf5_fp = self.hdf5_fp
             event_id, sta_id = trace_id.split("/")
+            waveform = hdf5_fp[trace_id][:, :].T  # [3, Nt]
 
-        waveform = hdf5_fp[trace_id][:, :].T  # [3, Nt]
+        # waveform = hdf5_fp[trace_id][:, :].T  # [3, Nt]
         waveform = normalize(waveform)
         nch, nt = waveform.shape
 
@@ -407,7 +415,10 @@ class SeismicTraceIterableDataset(IterableDataset):
         duration = []
         for e in event_ids:
             c0.append(np.mean(attrs["phase_index"][attrs["event_id"] == e]).item())
-            duration.append([np.min(attrs["phase_index"][attrs["event_id"] == e]).item(), np.max(attrs["phase_index"][attrs["event_id"] == e]).item()])
+            # duration.append([np.min(attrs["phase_index"][attrs["event_id"] == e]).item(), np.max(attrs["phase_index"][attrs["event_id"] == e]).item()])
+            tmp_min = np.min(attrs["phase_index"][attrs["event_id"] == e]).item()
+            tmp_max = np.max(attrs["phase_index"][attrs["event_id"] == e]).item()
+            duration.append([tmp_min, tmp_max + 2 * (tmp_max - tmp_min)])
         event_center, event_mask = generate_label([c0], nt=nt, label_width=self.event_width, return_mask=True)
         event_center = event_center[1, :]
 
@@ -539,8 +550,8 @@ class SeismicTraceIterableDataset(IterableDataset):
                 trace = stream[i]
 
             ## highpass filtering > 1Hz
-            trace = trace.detrend("constant")
-            trace = trace.filter("highpass", freq=1.0)
+            # trace = trace.detrend("constant")
+            # trace = trace.filter("highpass", freq=1.0)
     
             tmp.append(trace)
         stream = tmp

eqnet/models/unet.py
@@ -10,7 +10,7 @@ def log_transform(x):
     return x
 
 
-def normalize_local(data, filter=1024, stride=1):
+def normalize_local(data, filter=512, stride=1):
 
     nb, nch, nt, nx = data.shape
 
@@ -122,6 +122,7 @@ class UNet(nn.Module):
     def forward(self, x):
 
         bt, ch, nt, st = x.shape  # batch, channel, time, station
+        x = normalize_local(x)
         x = pad_input(x)
 
         if self.use_polarity:

predict.py
@@ -28,37 +28,38 @@ def pred_phasenet(model, data_loader, pick_path, figure_path, args):
 
             with torch.cuda.amp.autocast(enabled=args.amp):
                 output = model(meta)
-                if "phase" in output:
-                    phase_scores = torch.softmax(output["phase"], dim=1)  # [batch, nch, nt, nsta]
-                    if "polarity" in output:
-                        polarity_scores = (torch.sigmoid(output["polarity"]) - 0.5) * 2.0
-                    else:
-                        polarity_scores = None
-                    topk_phase_scores, topk_phase_inds = detect_peaks(phase_scores, vmin=args.min_prob, kernel=21)
-                    phase_picks_ = extract_picks(
-                        topk_phase_inds,
-                        topk_phase_scores,
-                        file_name=meta["file_name"],
-                        station_name=meta["station_name"],
-                        begin_time=meta["begin_time"] if "begin_time" in meta else None,
-                        dt=meta["dt_s"] if "dt_s" in meta else 0.01,
-                        vmin=args.min_prob,
-                        phases=args.phases,
-                        polarity_score=polarity_scores,
-                    )
+                
+            if "phase" in output:
+                phase_scores = torch.softmax(output["phase"], dim=1)  # [batch, nch, nt, nsta]
+                if "polarity" in output:
+                    polarity_scores = (torch.sigmoid(output["polarity"]) - 0.5) * 2.0
+                else:
+                    polarity_scores = None
+                topk_phase_scores, topk_phase_inds = detect_peaks(phase_scores, vmin=args.min_prob, kernel=21)
+                phase_picks_ = extract_picks(
+                    topk_phase_inds,
+                    topk_phase_scores,
+                    file_name=meta["file_name"],
+                    station_name=meta["station_name"],
+                    begin_time=meta["begin_time"] if "begin_time" in meta else None,
+                    dt=meta["dt_s"] if "dt_s" in meta else 0.01,
+                    vmin=args.min_prob,
+                    phases=args.phases,
+                    polarity_score=polarity_scores,
+                )
 
-                if "event" in output:
-                    event_scores = torch.sigmoid(output["event"])
-                    topk_event_scores, topk_event_inds = detect_peaks(event_scores, vmin=args.min_prob, kernel=21)
-                    event_picks_ = extract_picks(
-                        topk_event_inds,
-                        topk_event_scores,
-                        file_name=meta["file_name"],
-                        begin_time=meta["begin_time"] if "begin_time" in meta else None,
-                        dt=meta["dt_s"] if "dt_s" in meta else 0.01,
-                        vmin=args.min_prob,
-                        phases=["event"],
-                    )
+            if "event" in output:
+                event_scores = torch.sigmoid(output["event"])
+                topk_event_scores, topk_event_inds = detect_peaks(event_scores, vmin=args.min_prob, kernel=21)
+                event_picks_ = extract_picks(
+                    topk_event_inds,
+                    topk_event_scores,
+                    file_name=meta["file_name"],
+                    begin_time=meta["begin_time"] if "begin_time" in meta else None,
+                    dt=meta["dt_s"] if "dt_s" in meta else 0.01,
+                    vmin=args.min_prob,
+                    phases=["event"],
+                )
 
             for i in range(len(meta["file_name"])):
 
@@ -178,47 +179,14 @@ def main(args):
         utils.mkdir(figure_path)
 
     utils.init_distributed_mode(args)
-
-    device = torch.device(args.device)
-
-    model = eqnet.models.__dict__[args.model](
-        backbone=args.backbone, in_channels=1, out_channels=(len(args.phases) + 1), use_polarity=args.use_polarity
-    )
-
-    logger.info("Model:\n{}".format(model))
-
-    model.to(device)
-    if args.distributed:
-        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
-
-    model_without_ddp = model
-    if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
-        model_without_ddp = model.module
-
-    if args.resume:
-        checkpoint = torch.load(args.resume, map_location="cpu")
-        model_without_ddp.load_state_dict(checkpoint["model"], strict=False)
-        print("Loaded checkpoint '{}' (epoch {})".format(args.resume, checkpoint["epoch"]))
-    else:
-        if args.model == "phasenet" and (not args.use_polarity):
-            raise ("No pretrained model for phasenet, please use phasenet_polarity instead")
-        elif (args.model == "phasenet") and (args.use_polarity):
-            model_url = "https://github.com/AI4EPS/models/releases/download/PhaseNet-Polarity-v1/model_99.pth"
-        elif args.model == "phasenet_das":
-            model_url = "https://github.com/AI4EPS/models/releases/download/PhaseNet-DAS-v3/model_10.pth"
-        else:
-            raise
-        state_dict = torch.hub.load_state_dict_from_url(
-            model_url, model_dir="./", progress=True, check_hash=True, map_location="cpu"
-        )
-        model_without_ddp.load_state_dict(state_dict["model"], strict=False)
+    print(args)
 
     if args.distributed:
         rank = utils.get_rank()
         world_size = utils.get_world_size()
     else:
         rank, world_size = 0, 1
+    device = torch.device(args.device)
 
     if args.model == "phasenet":
         dataset = SeismicTraceIterableDataset(
@@ -258,6 +226,38 @@ def main(args):
         prefetch_factor=2,
         drop_last=False,
     )
+
+    model = eqnet.models.__dict__[args.model](
+        backbone=args.backbone, in_channels=1, out_channels=(len(args.phases) + 1), use_polarity=args.use_polarity
+    )
+    logger.info("Model:\n{}".format(model))
+
+    model.to(device)
+    if args.distributed:
+        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+
+    model_without_ddp = model
+    if args.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model_without_ddp = model.module
+
+    if args.resume:
+        checkpoint = torch.load(args.resume, map_location="cpu")
+        model_without_ddp.load_state_dict(checkpoint["model"], strict=True)
+        print("Loaded checkpoint '{}' (epoch {})".format(args.resume, checkpoint["epoch"]))
+    else:
+        if args.model == "phasenet" and (not args.use_polarity):
+            raise ("No pretrained model for phasenet, please use phasenet_polarity instead")
+        elif (args.model == "phasenet") and (args.use_polarity):
+            model_url = "https://github.com/AI4EPS/models/releases/download/PhaseNet-Polarity-v1/model_99.pth"
+        elif args.model == "phasenet_das":
+            model_url = "https://github.com/AI4EPS/models/releases/download/PhaseNet-DAS-v3/model_10.pth"
+        else:
+            raise
+        state_dict = torch.hub.load_state_dict_from_url(
+            model_url, model_dir="./", progress=True, check_hash=True, map_location="cpu"
+        )
+        model_without_ddp.load_state_dict(state_dict["model"], strict=True)
 
     if args.model == "phasenet":
         pred_phasenet(model, data_loader, pick_path, figure_path, args)

train.py
@@ -98,13 +98,12 @@ def plot_results(model, epoch, args, meta):
 
     with torch.inference_mode():
         if args.model == "phasenet":
-            with torch.no_grad():
-                out = model(meta)
-                phase = F.softmax(out["phase"], dim=1).cpu()
-                event = torch.sigmoid(out["event"]).cpu()
-                polarity = torch.sigmoid(out["polarity"]).cpu()
-                meta["waveform_raw"] = meta["waveform"].clone()
-                meta["waveform"] = normalize_local(meta["waveform"])
+            out = model(meta)
+            phase = torch.softmax(out["phase"], dim=1).cpu()
+            event = torch.sigmoid(out["event"]).cpu()
+            polarity = torch.sigmoid(out["polarity"]).cpu()
+            meta["waveform_raw"] = meta["waveform"].clone()
+            meta["waveform"] = normalize_local(meta["waveform"])
             print("Plotting...")
             eqnet.utils.visualize_phasenet_train(meta, phase, event, polarity, epoch=epoch, figure_dir=args.output_dir)
             del phase, event, polarity
@@ -113,10 +112,9 @@ def plot_results(model, epoch, args, meta):
             pass
 
         elif args.model == "phasenet_das":
-            with torch.no_grad():
-                preds = model(meta)
-                preds = F.softmax(preds, dim=1).cpu()
-                meta["data"] = normalize_local_das(meta["data"])
+            preds = model(meta)
+            preds = torch.softmax(preds, dim=1).cpu()
+            meta["data"] = normalize_local_das(meta["data"])
             print("Plotting...")
             eqnet.utils.visualize_das_train(meta, preds, epoch=epoch, figure_dir=args.output_dir)
             del preds
@@ -142,9 +140,13 @@ def main(args):
         utils.mkdir(args.output_dir)
 
     utils.init_distributed_mode(args)
-    rank = utils.get_rank() if args.distributed else 0
-    world_size = utils.get_world_size() if args.distributed else 1
     print(args)
+
+    if args.distributed:
+        rank = utils.get_rank()
+        world_size = utils.get_world_size()
+    else:
+        rank, world_size = 0, 1
 
     device = torch.device(args.device)
 
@@ -155,7 +157,22 @@ def main(args):
     #     train_sampler = torch.utils.data.RandomSampler(dataset)
     #     test_sampler = torch.utils.data.SequentialSampler(dataset_test)
 
-    if args.model == "phasenet_das":
+    if args.model == "phasenet":
+        dataset = SeismicTraceIterableDataset(
+            data_path=args.data_path,
+            data_list=args.data_list,
+            hdf5_file=args.hdf5_file,
+            format="h5",
+            training=True,
+            stack_event=args.stack_event,
+            flip_polarity=args.flip_polarity,
+            rank=rank,
+            world_size=world_size,
+        )
+        train_sampler = None
+        dataset_test = dataset
+        test_sampler = None
+    elif args.model == "phasenet_das":
         dataset = DASIterableDataset(
             # data_path="/net/kuafu/mnt/tank/data/EventData/Mammoth_north/data",
             # noise_path="/net/kuafu/mnt/tank/data/EventData/Mammoth_north/data",
@@ -220,21 +237,8 @@ def main(args):
         train_sampler = None
         dataset_test = dataset
         test_sampler = None
-    elif args.model == "phasenet":
-        dataset = SeismicTraceIterableDataset(
-            data_path=args.data_path,
-            data_list=args.data_list,
-            hdf5_file=args.hdf5_file,
-            format="h5",
-            training=True,
-            stack_event=args.stack_event,
-            flip_polarity=args.flip_polarity,
-            rank=rank,
-            world_size=world_size,
-        )
-        train_sampler = None
-        dataset_test = dataset
-        test_sampler = None
+    else:
+        raise ("Unknown model")
 
     data_loader = torch.utils.data.DataLoader(
         dataset,
@@ -253,7 +257,6 @@ def main(args):
         sampler=test_sampler,
         num_workers=args.workers,
         collate_fn=None,
-        # collate_fn=utils.collate_fn
     )
 
     model = eqnet.models.__dict__[args.model](
@@ -273,9 +276,7 @@ def main(args):
 
     model_without_ddp = model
     if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(
-            model, device_ids=[args.gpu]
-        )  # , find_unused_parameters=True)
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
         model_without_ddp = model.module
 
     params_to_optimize = [p for p in model_without_ddp.parameters() if p.requires_grad]