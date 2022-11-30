41596dde1adad4d9b78625d2eb6a654fca2dcd56
Weiqiang <zhuwq0@gmail.com>
2022-11-29 23:23:01 -0800

    update filt_3c.py

--- datasets/NCEDC/filt_3c.py
+++ datasets/NCEDC/filt_3c.py
@@ -35,6 +35,7 @@ def calc_snr(waveform, picks, noise_window=300, signal_window=300, gap_window=50
 # %%
 h5_in = "ncedc_event_dataset.h5"
 h5_out = "ncedc_event_dataset_3c.h5"
+num = 0
 with h5py.File(h5_out, "w") as fp_out:
     with h5py.File(h5_in, "r") as fp_in:
         for event_id, event in tqdm(fp_in.items()):
@@ -52,25 +53,27 @@ with h5py.File(h5_out, "w") as fp_out:
                 SNR = calc_snr(waveform, P_index)
                 # S_SNR = calc_snr(waveform, S_index)
 
-                if (np.all(np.array(SNR) > 0)) and (np.max(np.array(SNR)) > 2.0):
-                    if event_id not in fp_out:
-                        fp_out.create_group(event_id)
-                        for key, value in event.attrs.items():
-                            fp_out[event_id].attrs[key] = value
-                    if station_id not in fp_out[event_id]:
-                        fp_out[event_id].create_dataset(station_id, data=waveform)
-                        for key, value in station.attrs.items():
-                            fp_out[event_id][station_id].attrs[key] = value
-                        fp_out[event_id][station_id].attrs["snr"] = SNR
-                    else:
-                        print("Duplicate station", event_id, station_id)
-                
+                if len(SNR) >= 3:
+                    if (np.all(np.array(SNR) > 0)) and (np.max(np.array(SNR)) > 2.0):
+                        if event_id not in fp_out:
+                            fp_out.create_group(event_id)
+                            for key, value in event.attrs.items():
+                                fp_out[event_id].attrs[key] = value
+                        if station_id not in fp_out[event_id]:
+                            fp_out[event_id].create_dataset(station_id, data=waveform)
+                            for key, value in station.attrs.items():
+                                fp_out[event_id][station_id].attrs[key] = value
+                            fp_out[event_id][station_id].attrs["snr"] = SNR
+                            num += 1
+                        else:
+                            print("Duplicate station", event_id, station_id)
+                    
                 #     print("3C", SNR)
                 #     # continue
                 #     raise
                 # else:
                 #     print("xC", SNR)
                 #     continue
-
+print(f"Total number of samples: {num}")
 
 # %%
